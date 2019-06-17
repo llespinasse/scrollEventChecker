af6f1503b214f500c07f372cfd224da0598fda6c
llespinasse <44224218+llespinasse@users.noreply.github.com>
2019-06-17 09:10:19 +0200

    Update core-lib.js

--- core-lib.js
+++ core-lib.js
@@ -1,14 +1,14 @@
 var srollAnimation = function(obj) {
-  var className 	= (obj.className != undefined) ? obj.className : 'scroll-obj';
-  var gap 	= (obj.gap != undefined) ? obj.gap : 100;
+  var className = (obj.className != undefined) ? obj.className : 'scroll-obj';
+  var gap = (obj.gap != undefined) ? obj.gap : 100;
   var callback 	= (obj.callback != undefined) ? obj.callback : function() {};
-  var elmts = jQuery('.' + className);
-  jQuery(window).on('scroll', checkOffset);
-  jQuery(window).on('resize', checkOffset);
+  var elmts = $('.' + className);
+  $(window).on('scroll', checkOffset);
+  $(window).on('resize', checkOffset);
   function checkOffset() {
-    var scroll = jQuery(window).scrollTop();
+    var scroll = $(window).scrollTop();
     for(var i = 0; i < elmts.length; i++) {
-      if(jQuery(elmts[i]).offset().top - scroll <= jQuery(window).height() - gap) {
+      if($(elmts[i]).offset().top - scroll <= $(window).height() - gap) {
         callback(elmts);
       }
     }
@@ -18,5 +18,9 @@ var srollAnimation = function(obj) {
 var myAnimation = new srollAnimation ({
   className: 'desc-box',
   gap: 100,
-  callback: animPannel,
+  callback: callbackFunc,
 });
+
+function callbackFunc(elmts) {
+  $(this).show();
+}
